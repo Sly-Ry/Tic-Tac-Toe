e4b8a4df1e4b45b16a8d12053d579613c0295d99
ryan <ryan.williams93@aol.com>
2022-08-20 10:12:26 -0500

    printed gameboard

--- ticTac_java/TicTacToe.java
+++ ticTac_java/TicTacToe.java
@@ -3,10 +3,20 @@ public class TicTacToe {
         
         // 2d array
         // chars used for characters
+        // 5 rows with columns
         char[][] gameboard = {{' ', '|', ' ', '|', ' '}, 
             {'-', '+', '-', '+', '-'},
             {' ', '|', ' ', '|', ' '},
             {'-', '+', '-', '+', '-'},
             {' ', '|', ' ', '|', ' '}};
+        
+        // prints out gameboard (using shorthand)
+        for(char[] row : gameboard){
+            for(char c : row){
+                System.out.print(c);
+            }
+            // after each row, print line
+            System.out.println();
+        }
     }
 }
