34b537382bb3ed87d840882f82cd5444d269492c
ryan <ryan.williams93@aol.com>
2022-08-20 10:00:33 -0500

    create inital gameboard

--- ticTac_java/TicTacToe.java
+++ ticTac_java/TicTacToe.java
@@ -1,5 +1,12 @@
 public class TicTacToe {
     public static void main(String[] args){
         
+        // 2d array
+        // chars used for characters
+        char[][] gameboard = {{' ', '|', ' ', '|', ' '}, 
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '}};
     }
 }
