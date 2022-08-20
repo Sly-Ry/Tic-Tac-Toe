27cfe56e3286df0c0617fff0911b968416499952
ryan <ryan.williams93@aol.com>
2022-08-20 17:29:06 -0500

    initialized gameboard with proper function call

--- ticTac_java/TicTacToe.java
+++ ticTac_java/TicTacToe.java
@@ -1,3 +1,5 @@
+import java.lang.Thread.State;
+
 public class TicTacToe {
     public static void main(String[] args){
         
@@ -9,8 +11,12 @@ public class TicTacToe {
             {' ', '|', ' ', '|', ' '},
             {'-', '+', '-', '+', '-'},
             {' ', '|', ' ', '|', ' '}};
+            
+            printGameBoard(gameboard);
+    }
         
         // prints out gameboard (using shorthand)
+        public static void printGameBoard(char[][] gameboard) {
         for(char[] row : gameboard){
             for(char c : row){
                 System.out.print(c);
