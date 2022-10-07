032725def2ea6c98aec3d6d175e2c03d237c5ea3
ryan <ryan.williams93@aol.com>
2022-10-07 17:53:49 -0500

    selector elements created in js

---
+++ WeatherV3/index.js
@@ -0,0 +1,20 @@
+// DOM elements
+const weather = document.querySelector(".weather");
+const temp = document.querySelector(".temp");
+const date = document.querySelector(".date");
+const time = document.querySelector(".time");
+const condition = document.querySelector(".time");
+const name = document.querySelector(".name");
+const icon = document.querySelector(".icon");
+const cloud = document.querySelector(".cloud");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+const form = document.getElementById("search-bar");
+const search = document.querySelector(".search");
+const btn = document.querySelector(".submit");
+const cities = document.querySelectorAll('.city');
+
+// Default loading city
+let cityInput = "Tokyo";
+
+// Click event per city
