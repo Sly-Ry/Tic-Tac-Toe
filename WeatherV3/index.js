f11eda05073bb16682d20389f7b9785591f07889
ryan <ryan.williams93@aol.com>
2022-10-08 18:36:39 -0500

    incorporated alternate js functions

--- WeatherV3/index.js
+++ WeatherV3/index.js
@@ -1,26 +1,148 @@
-// DOM elements
-const weather = document.querySelector(".weather");
-const temp = document.querySelector(".temp");
-const date = document.querySelector(".date");
-const time = document.querySelector(".time");
-const condition = document.querySelector(".time");
-const name = document.querySelector(".name");
-const icon = document.querySelector(".icon");
-const cloud = document.querySelector(".cloud");
-const humidity = document.querySelector(".humidity");
-const wind = document.querySelector(".wind");
-const form = document.getElementById("search-bar");
-const search = document.querySelector(".search");
-const btn = document.querySelector(".submit");
-const cities = document.querySelectorAll('.city');
+let fetchWeather = {
+    "apiKey": "2632ab542fff737012a28d74931b6af5",
+    getWeather: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => 
+        this.displayWeather(data));
+    },
+    // display weather for searched city 
+    displayWeather: function(data){
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        const { all } = data.clouds;
 
-// Default loading city
-let cityInput = "Austin";
+        document.querySelector(".name").innerText = name
+        document.querySelector(".icon").src = 
+            "https://openweathermap.org/img/wn/" + icon + ".png"
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".cloud").innerText = all + "%";
+        document.querySelector(".temp").innerText = Math.floor(temp) + "°C"
+        document.querySelector(".humidity").innerText = humidity +"%"
+        document.querySelector(".wind").innerText = speed + " km/h"
+        
+        // document.querySelector(".weather").classList.remove("loading");
 
-// Click event per city
-cities.forEach((city, (e) => {
-    // Changes default to clicked
-    cityInput = e.target.innerHTML;
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?'" + name + "')";
+    },
+    search: function() {
+        this.getWeather(document.querySelector(".search-bar").value);
+    }
+};
 
-    // Fetch and display weather using the Weather API
-}));
+// Button Search
+document.querySelector(".search button").addEventListener("click", function() {
+    fetchWeather.search();
+});
+
+// "Enter" Search
+document.querySelector(".search-bar").addEventListener('keyup', function(event) {
+    if (event.key == "Enter") {
+        fetchWeather.search();
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+// ///////
+// const apiKey = "2632ab542fff737012a28d74931b6af5";
+// // DOM elements
+// const weather = document.querySelector(".weather");
+// const temp = document.querySelector(".temp");
+// const date = document.querySelector(".date");
+// const time = document.querySelector(".time");
+// const condition = document.querySelector(".time");
+// const name = document.querySelector(".name");
+// const icon = document.querySelector(".icon");
+// const cloud = document.querySelector(".cloud");
+// const humidity = document.querySelector(".humidity");
+// const wind = document.querySelector(".wind");
+// const form = document.getElementById("search-bar");
+// const search = document.querySelector(".search");
+// const btn = document.querySelector(".submit");
+// const cities = document.querySelectorAll('.city');
+
+// // Default loading city
+// let cityInput = "Austin";
+
+// // Click event per city
+// cities.forEach((city) =>{
+//     city.addEventListener("click", (e) => {
+//         // Changes default to clicked
+//         cityInput = e.target.innerHTML;
+
+//         // Fetch and display weather using the Weather API
+//         fetchWeather()
+
+//         // fancy styling: Fade out
+//         app.style.opacity = '0';
+//     });
+// });
+
+// // Form btn event click
+// form.addEventListener('submit', (e) => {
+//     // error handling: no user input upon submit
+//     if (search.value.length == 0) {
+//         alert("Enter city name");
+//     }
+//     else {
+//         // changes default city to searched value
+//         cityInput = search.value;
+
+//         //  initialize fetch call
+//         fetchWeather();
+
+//         //  clears search bar
+//         search.value = "";
+
+//         //  fade out styling
+//         app.style.opacity = "0";
+//     }
+
+//     e.preventDefault();
+// });
+
+// // gets current Date
+// function currentDate(day, month, year) {
+//     const weekday = [
+//         "Monday",
+//         "Tuesday",
+//         "Wednesday",
+//         "Thursday",
+//         "Friday",
+//         "Saturday",
+//         "Sunday"
+//     ];
+
+//     return weekday[new Date(`${day}/${month}/${year}`).getDate()];
+// }
+
+// // function that fetches weather data
+// function fetchWeather() {
+//     fetch(
+//             "https://api.openweathermap.org/data/2.5/weather?q=" 
+//             + `${cityInput}`  
+//             + "&units=metric&appid=" 
+//             + this.apiKey
+//         )
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log(data);
+
+//         }
+// };
