14c79de7ef28a6226d9a8ab423fd29c16bf678ab
ryan <ryan.williams93@aol.com>
2022-10-23 17:05:09 -0500

    adjusted date script to reflect current date

--- WeatherV3/index.js
+++ WeatherV3/index.js
@@ -1,14 +1,104 @@
 const app = document.querySelector('.weather');
-const dateData = document.querySelector('.date');
 const time = document.querySelector('.time');
 const cities = document.querySelectorAll('.city');
+
+// gets date
+let date = new Date()
+
+let day = getDate(date.getDay());
+let month = getMonth(date.getMonth());
+let monthNum = date.getMonth() + 1;
+let year = date.getFullYear(); 
+
+console.log(year);
+
+document.querySelector('.date').innerHTML = `${day} ` + `${monthNum} `+ `${month} `  + `${year}`;
+
+function getDate(n) {
+    console.log(n);
+    switch (n) {
+        case 0:
+            name = "S";
+          break;
+        case 1:
+            name = "M";
+          break;
+        case 2:
+            name = "T";
+          break;
+        case 3:
+            name = "W";
+          break;
+        case 4:
+            name = "Th";
+          break;
+        case 5:
+            name = "F";
+          break;
+        case 6:
+            name = "St";
+    }
+    return name;
+};
+
+function getMonth(n) {
+   
+    switch(n) {
+        case 1: 
+            name = "Jan"
+          break;
+        case 2: 
+            name = "Feb"
+          break;
+        case 3: 
+            name = "Mar"
+            break;
+        case 4: 
+            name = "Apr"
+            break;
+        case 5: 
+            name = "May"
+            break;
+        case 6: 
+            name = "Jun"
+            break;
+        case 7: 
+            name = "Jul"
+            break;
+        case 8: 
+            name = "Aug"
+            break;
+        case 9: 
+            name = "Sept"
+            break;
+        case 10: 
+            name = "Oct"
+            break;
+        case 11: 
+            name = "Nov"
+            break;
+        case 12: 
+            name = "Dec"
+    }
+    return name;
+};
+
+
+// Current Time
+var myVar = setInterval(function() {
+    myTimer();
+}, 1000);
+  
+function myTimer() {
+    var d = new Date();
+    document.querySelector(".time").innerHTML = d.toLocaleTimeString();
+}
 
 // Click event per city
 cities.forEach((city) =>{
     city.addEventListener("click", (e) => {
         // Changes default to clicked
         c = e.target.innerHTML;
-        console.log(c);
 
         // Fetch and display weather using the Weather API
         fetchWeather.getWeather(c);
@@ -33,6 +123,7 @@ function dayOfTheWeek(day, month, year) {
     return weekday[new Date(`${day}/${month}/${year}`).getDate()];
 }
 
+
 let fetchWeather = {
 
     "apiKey": "2632ab542fff737012a28d74931b6af5",
@@ -47,12 +138,8 @@ let fetchWeather = {
         )
         .then((response) => response.json())
         .then((data) => 
-        this.displayWeather(data));
-
-        const date = date.location.localTime;
-
-
-        
+            this.displayWeather(data)
+        );
     },
     // display weather for searched city 
     displayWeather: function(data){
@@ -72,6 +159,29 @@ let fetchWeather = {
         document.querySelector(".temp").innerText = Math.floor(temp) + "°C"
         document.querySelector(".humidity").innerText = humidity +"%"
         document.querySelector(".wind").innerText = speed + " km/h"
+
+        // Searched Time
+        let unix_timestamp = data.dt;
+        console.log(unix_timestamp);
+
+        // Create a new JavaScript Date object based on the timestamp
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        var date = new Date(unix_timestamp * 1000);
+        
+        // Hours part from the timestamp
+        var hours = date.getHours();
+
+        // Minutes part from the timestamp
+        var minutes = "0" + date.getMinutes();
+
+        // Seconds part from the timestamp
+        var seconds = "0" + date.getSeconds();
+
+
+        // Will display time in 10:30:23 format
+        document.querySelector(".time").innerHTML = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+        console.log(document.querySelector(".time").innerHTML);
 
         this.backgroundImageChange(description);
 
